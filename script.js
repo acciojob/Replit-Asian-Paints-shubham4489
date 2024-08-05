3e5aff6920e42ae19a9bec17c4c2022802ebd7f7
shubham4489 <143184073+shubham4489@users.noreply.github.com>
2024-08-05 14:32:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const gridContainer = document.querySelector(".grid");
+const form =document.querySelector("#form");
+let previousSelectedElement =null;
+
+form.addEventListener("submit", function (event){
+	event.preventDefault();
+
+	let cellId =form.inputId.value;
+	let selectedColor =form.color.value;
+
+
+	let cell =document.getElementById(cellId);
+	cell.style.backgroundColor= selectedColor;
+	if(previousSelectedElement){
+		previousSelectedElement.style.backgroundColor= "transparent";	
+	}
+    previousSelectedElement= cell;
+});
+
+
+for(let i=1; i<=9; i++){
+	const gridItem =document.createElement("div");
+	gridItem.id=i;
+	gridItem.innerText=i;
+	gridItem.className ="grid-item";
+	gridContainer.appendChild(gridItem);
+}
